5cf3749ce42feb1f0801bafc315a86822ae3d4f9
Vaibhav Bhapkar <vaibhavrb999@gmail.com>
2021-04-05 20:36:37 +0530

    Refactoring of code

--- TypewiseAlert/FindTypeInAssembly.cs
+++ TypewiseAlert/FindTypeInAssembly.cs
@@ -9,7 +9,7 @@ namespace TypewiseAlert
     {
         public static Type FindInstanceOfAssembly(string sourceName, string typeName)
         {
-            Type type = FindInstanceInExecutingAssembly(sourceName,typeName);
+            Type type = FindInstanceInExecutingAssembly(sourceName, typeName);
             if (type == null)
             {
                 type = FindInstanceInReferencedAssemblies(sourceName, typeName);
@@ -17,19 +17,19 @@ namespace TypewiseAlert
             return type;
         }
 
-        private static Type FindInstanceInExecutingAssembly(string sourceName,string typeName)
+        private static Type FindInstanceInExecutingAssembly(string sourceName, string typeName)
         {
             AssemblyName[] assemblyNames = new AssemblyName[1];
             AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
             assemblyNames[0] = assemblyName;
-            return CheckTypeInAssembly(assemblyNames, sourceName,typeName);
+            return CheckTypeInAssembly(assemblyNames, sourceName, typeName);
         }
-        private static Type FindInstanceInReferencedAssemblies(string sourceName,string typeName)
+        private static Type FindInstanceInReferencedAssemblies(string sourceName, string typeName)
         {
             AssemblyName[] assemblyNames = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
-            return CheckTypeInAssembly(assemblyNames, sourceName,typeName);
+            return CheckTypeInAssembly(assemblyNames, sourceName, typeName);
         }
-        private static Type CheckTypeInAssembly(AssemblyName[] assemblyNames, string sourceName,string typeName)
+        private static Type CheckTypeInAssembly(AssemblyName[] assemblyNames, string sourceName, string typeName)
         {
             foreach (var assemblyName in assemblyNames)
             {
@@ -50,20 +50,14 @@ namespace TypewiseAlert
         }
         public static List<object> GetInstanceFromInterface(string typeName)
         {
-            AssemblyName[] assemblyNames = new AssemblyName[1];
-            AssemblyName executingAssemblyName = Assembly.GetExecutingAssembly().GetName();
-            assemblyNames[0] = executingAssemblyName;
             List<object> instanceList = new List<object>();
-            foreach (var assemblyName in assemblyNames)
+            Assembly currentAssembly = Assembly.Load(Assembly.GetExecutingAssembly().GetName());
+            Type[] types = currentAssembly.GetTypes();
+            for (int i = 0; i < types.Length; i++)
             {
-                Assembly assembly = Assembly.Load(assemblyName);
-                Type[] types = assembly.GetTypes();
-                foreach (var type in types)
+                if (types[i].GetInterface(typeName) != null)
                 {
-                    if (type.GetInterface(typeName) != null)
-                    {
-                        instanceList.Add(Activator.CreateInstance(type));
-                    }
+                    instanceList.Add(Activator.CreateInstance(types[i]));
                 }
             }
             return instanceList;
